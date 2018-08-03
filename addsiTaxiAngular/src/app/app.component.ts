58625313425482bf4a959c94f892aec3635249a1
AbelTilahunMeko <at351291@gmail.com>
2018-08-03 09:26:06 +0300

    commiting after practicing

--- addsiTaxiAngular/src/app/app.component.ts
+++ addsiTaxiAngular/src/app/app.component.ts
@@ -8,5 +8,67 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  twoWayBinding = ''
-} 
+  allowedToTouch = false;
+  fName: String = 'Abel Tilahun';
+  departmentName: String = 'Software Engineering';
+  serverStatus = 'there is no server';
+
+  initialPhoneNUmber: any = 'please enter your number';
+
+  userNameGotten: String = '';
+  passwordGotten: String = '';
+
+  userName: String = 'Abel';
+  password: String = '1234';
+
+  showString: String = 'Please fill in the forms';
+
+
+  successfulEnter = false;
+
+  constructor() {
+    setTimeout( () => {
+      this.allowedToTouch  = true;
+    }, 2000);
+  }
+  sendUserInput(event: Event) {
+    this.userNameGotten = (<HTMLInputElement>event.target).value.toString();
+  }
+
+  sendUserPassword(event: Event) {
+    this.passwordGotten =  (<HTMLInputElement>event.target).value.toString();
+  }
+
+  displayAllInputs() {
+    return this.showString;
+  }
+
+  successful() {
+    console.log(this.userName.toString() + this.password.toString());
+    console.log(this.passwordGotten.toString() + this.userNameGotten.toString());
+
+    this.showString = this.userNameGotten.toString() + this.passwordGotten.toString() + this.initialPhoneNUmber.toString();
+    if (this.userName.toString() === this.userNameGotten.toString() && this.password.toString() === this.passwordGotten.toString()) {
+      return this.successfulEnter = true;
+    } else {
+      return false;
+    }
+    // return this.successfulEnter = true;
+  }
+  seeTheUserInputs() {
+    return this.userNameGotten.concat(this.passwordGotten.toString());
+  }
+  onCreateSomething() {
+    return this.serverStatus = 'server was created';
+  }
+
+  // Creating a method bro
+  getFName() {
+    return this.fName;
+  }
+
+  getDepartment() {
+    return this.departmentName;
+  }
+
+}
--- addsiTaxiAngular/src/app/app.module.ts
+++ addsiTaxiAngular/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import 'materialize-css';
-import { MaterializeModule } from "angular2-materialize";
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
+// This are the component that are going to be declared
+// Remember when adding a component you should never include the .ts
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
